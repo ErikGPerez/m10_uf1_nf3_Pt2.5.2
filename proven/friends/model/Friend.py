fc397da9c6eb628180b9df62b7cf677370d1df71
ErikGPerez <erikgranadoperez2@gmail.com>
2023-10-28 17:43:23 +0200

    Update del anterior

---
+++ proven/friends/model/Friend.py
@@ -0,0 +1,12 @@
+
+
+
+class Friend:
+    
+    def __init__(self, phone, name, age):
+        self.phone = phone
+        self.name = name
+        self.afe = age
+        
+        #TODO Hacer la clase constructora. 
+        
